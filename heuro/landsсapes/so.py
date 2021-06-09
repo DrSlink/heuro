aa7929ef43aab4448978f9f0a85a4b6fac310470
Ekaterina <ketty98@mail.ru>
2021-06-09 18:39:04 +0300

    added two new functions: rosenbrock and sphere. directory was renamed

---
+++ heuro/landsсapes/so.py
@@ -0,0 +1,21 @@
+from math import cos, pi
+
+
+def rastrigin(x, a=10):
+    res = a * len(x)
+    for x_i in x:
+        res += x_i ** 2 - a * cos(2 * pi * x_i)
+    return res
+
+def rosenbrock(x, a=1, b=100):
+    res = 0
+    for i in range(len(x) - 1):
+        res += (a - x[i]) ** 2 + b * (x[i + 1] - x[i] ** 2) ** 2
+    return res
+
+
+def sphere(x):
+    res = 0
+    for x_i in x:
+        res += x_i ** 2
+    return res
