564d23cc3094a15e86086cf1723859fedc01f6fe
Aleksandr Poslavsky <slink21@mail.ru>
2021-06-10 18:00:02 +0300

    updated hill_climbing to create generators

--- heuro/population/hill_climbing.py
+++ heuro/population/hill_climbing.py
@@ -2,30 +2,35 @@ import random
 
 
 class SimpleHC:
-    def __init__(self, seed=None):
+    def __init__(self, seed=None, callback=None):
         self.random = random.Random(seed)
+        self.callback = callback
 
     def mutate(self, vector, constr):
         new_vec = []
         mutation_prob = 1 / len(vector)
         for i in range(len(vector)):
             if self.random.random() < mutation_prob:
-                new_vec.append(self.random.triangular(constr[i][0], constr[i][1], vector[i]))
+                new_vec.append(self.random.triangular(constr[i][0],
+                                                      constr[i][1], vector[i]))
             else:
                 new_vec.append(vector[i])
         return new_vec
 
-    def fit(self, func, constr, steps_without_improve=100):
+    def fit(self, func, constr, steps_without_improve=10):
         best_vec = []
         for con in constr:
             best_vec.append(self.random.uniform(con[0], con[1]))
         best_val = func(best_vec)
+        yield best_vec, best_val, best_vec, best_val, True
         made_steps = 0
         while made_steps != steps_without_improve:
             current_vec = self.mutate(best_vec, constr)
             current_val = func(current_vec)
             made_steps += 1
-            if current_val < best_val:
+            is_better = current_val < best_val
+            yield best_vec, best_val, current_vec, current_val, is_better
+            if is_better:
                 best_val = current_val
                 best_vec = current_vec
                 made_steps = 0
@@ -36,11 +41,13 @@ class ShotgunHC:
     def __init__(self, seed=None):
         self.simple_hc = SimpleHC(seed=seed)
 
-    def fit(self, *args, restarts_without_improve=100, **kwargs):
-        best_vec, best_val = self.simple_hc.fit(*args, **kwargs)
+    def fit(self, *args, restarts_without_improve=10, **kwargs):
+        gen = self.simple_hc.fit(*args, **kwargs)
+        best_vec, best_val = yield from gen
         made_steps = 0
         while made_steps != restarts_without_improve:
-            current_vec, current_val = self.simple_hc.fit(*args, **kwargs)
+            gen = self.simple_hc.fit(*args, **kwargs)
+            current_vec, current_val = yield from gen
             made_steps += 1
             if current_val < best_val:
                 best_val = current_val
